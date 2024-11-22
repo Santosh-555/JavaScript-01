a1e5061310cb4c6754a42421d5a3a0c666bba0ec
R Santosh Kumar Mudaliar <100425448+Santosh-555@users.noreply.github.com>
2024-11-22 14:39:07 +0000

    Updated Variables File

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 260899;
+let accountEmail = "santosh555@gmail.com";
+var accountPassword = "260835";
+accountCity = "Mumbai";
+let accountState;
+
+// accountId = 260840 // Not Allowed
+
+accountEmail = "santosh505@gmail.com";
+
+accountPassword = "260840";
+
+accountCity = "Gujarat";
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+// It is advisable to avoid using 'var' due to its limitations in block and functional scoping, which can lead to unexpected behaviors and potential bugs.
